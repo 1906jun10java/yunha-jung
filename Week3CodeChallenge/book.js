6f74f986a185f6c7385e6791d8f60a80fb657840
YunHa Jung <jungyunha1994@live.com>
2019-07-15 14:15:17 -0400

    Update book.js

--- Week3CodeChallenge/book.js
+++ Week3CodeChallenge/book.js
@@ -25,7 +25,6 @@ class Book {
         <td>${book.author}</td>
         <td>${book.genre}</td>
         <td>${book.coverimage}</td>
-        <td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>
       `;
   
       list.appendChild(row);
